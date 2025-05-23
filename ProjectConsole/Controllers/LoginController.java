fae7de48b48830f76fdcd2b89dbb8ed0bbb86fb5
adambakri-dev <adam.bakri2018@gmail.com>
2025-05-23 20:45:00 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	ProjectConsole/Controllers/ProfileCotroller.java

--- ProjectConsole/Controllers/LoginController.java
+++ ProjectConsole/Controllers/LoginController.java
@@ -2,10 +2,29 @@ package ProjectConsole.Controllers;
 
 import ProjectConsole.DataBase.UsersDataBase;
 import ProjectConsole.Users.Student;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+
+import java.io.File;
+import java.io.IOException;
 
 import java.util.Scanner;
 
 public class LoginController {
+    private Button RegisterButton;
     private UsersDataBase db;
     ChooserRoleController chooserRoleController=new ChooserRoleController(db);
     public LoginController(UsersDataBase db){
@@ -22,6 +41,25 @@ public class LoginController {
             Student student=db.GetUserByID(enteredid);
             chooserRoleController.ChRole(student);
         }
-
     }
+    @FXML
+    public void showWelcomeWindow() {
+        System.out.println("hello from button");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ProjectConsole/UI/RegisterUI.fxml"));
+            // استبدل "/path/to/your/fxml/Welcome.fxml" بالمسار الصحيح لملف الـFXML داخل resources
+
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Welcome");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
